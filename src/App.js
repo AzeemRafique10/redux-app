61ff35fdb5ee622173bbc1fe2991d1488c046a5b
Azeem Rafique <urbancrime19@gmail.com>
2024-12-05 14:14:19 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+// import Home from "./components/Home";
+// import User from "./Props/User";
+import HomeContainer from "./containers/HomeContainer";
+
+const App = () => {
+  return (
+    <div className="App">
+      {/* <User data={{ name: "Azeem", last: "Rafique", age: 26 }} /> */}
+      {/* <Home /> */}
+
+      <HomeContainer/>
+    </div>
+  );
+}
+
+export default App;
